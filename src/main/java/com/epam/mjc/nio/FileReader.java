db560bf482f32839d56d2d2f35b60b5a4e657808
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-08 07:46:50 +0500

    Java nio library

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 public class FileReader {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        Path wiki_path = Paths.get("src/main/resources/Profile.txt");
+        fileReader.getDataFromFile(wiki_path.toFile());
+    }
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Charset charset = Charset.forName("ISO-8859-1");
+        String[] cleanedData = new String[4];
+        try {
+            List<String> lines = Files.readAllLines(file.toPath(), charset);
+            System.out.println(lines);
+            int i = 0;
+            for (String line : lines) {
+                String[] currentData = line.split(":");
+                cleanedData[i] = currentData[1].strip();
+                i++;
+            }
+        }
+        catch (IOException e) {
+            e.toString();
+        }
+        return new Profile(
+                cleanedData[0],
+                Integer.valueOf(cleanedData[1]),
+                cleanedData[2],
+                Long.parseLong(cleanedData[3])
+        );
     }
 }
