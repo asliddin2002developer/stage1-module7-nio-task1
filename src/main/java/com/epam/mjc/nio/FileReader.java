d9dc6e6d840b078e6c47e1135ce0460f0b3e9187
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-08 07:56:17 +0500

    Java nio library updated

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -4,24 +4,21 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 
 public class FileReader {
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        Path wiki_path = Paths.get("src/main/resources/Profile.txt");
-        fileReader.getDataFromFile(wiki_path.toFile());
+        File file = new File("src/main/resources/Profile.txt");
+        fileReader.getDataFromFile(file);
     }
 
     public Profile getDataFromFile(File file) {
-        Charset charset = Charset.forName("ISO-8859-1");
         String[] cleanedData = new String[4];
         try {
+            Charset charset = Charset.forName("ISO-8859-1");
             List<String> lines = Files.readAllLines(file.toPath(), charset);
-            System.out.println(lines);
             int i = 0;
             for (String line : lines) {
                 String[] currentData = line.split(":");
