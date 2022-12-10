57b0c08e83412794c08f194cd09cb0d12be16b89
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-10 08:02:18 +0500

    java io updated

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,26 +1,33 @@
 package com.epam.mjc.nio;
 
 import java.io.*;
+import java.nio.Buffer;
 
 public class FileReader {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         FileReader fileReader = new FileReader();
         File file = new File("src/main/resources/Profile.txt");
         fileReader.getDataFromFile(file);
     }
 
-    public Profile getDataFromFile(File file) throws IOException {
+    public Profile getDataFromFile(File file) {
         String[] cleanedData = new String[4];
-        java.io.FileReader fileReader = new java.io.FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String n;
-        int i = 0;
-        while ((n = bufferedReader.readLine()) != null){
-            String data = n.split(":")[1].strip();
-            cleanedData[i] = data;
-            i++;
+        java.io.FileReader fileReader = null;
+        try {
+            fileReader = new java.io.FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String n;
+            int i = 0;
+            while ((n = bufferedReader.readLine()) != null){
+                String data = n.split(":")[1].strip();
+                cleanedData[i] = data;
+                i++;
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        fileReader.close();
+
 
         return new Profile(
                 cleanedData[0],
