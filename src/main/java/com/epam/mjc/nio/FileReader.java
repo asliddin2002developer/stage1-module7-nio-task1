26f050b986fd5e6d4a753f685193f6c36024b851
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-10 08:33:17 +0500

    java io updated logic

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,6 +1,10 @@
 package com.epam.mjc.nio;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
-import java.io.*;
 
 public class FileReader {
     public static void main(String[] args) {
@@ -10,39 +14,39 @@ public class FileReader {
     }
 
     public Profile getDataFromFile(File file) {
-        String[] cleanedData = new String[4];
-        java.io.FileReader fileReader;
-        BufferedReader bufferedReader;
+        StringBuilder bld = new StringBuilder();
 
-        try {
-            fileReader = new java.io.FileReader(file);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        bufferedReader = new BufferedReader(fileReader);
-        String n;
-        int i = 0;
-        while (true) {
-            try {
-                if ((n = bufferedReader.readLine()) == null) break;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        try (RandomAccessFile aFile = new RandomAccessFile(file.getPath(), "r");
+             FileChannel inChannel = aFile.getChannel()) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (inChannel.read(buffer) > 0) {
+                buffer.flip();
+                for (int i = 0; i < buffer.limit(); i++) {
+                    bld.append((char) buffer.get());
+                }
+                buffer.clear(); // do something with the data and clear/compact it.
             }
-            String data = n.split(":")[1].strip();
-            cleanedData[i] = data;
-            i++;
-        }
-        try {
-            bufferedReader.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-        return new Profile(
-                cleanedData[0],
-                Integer.valueOf(cleanedData[1]),
-                cleanedData[2],
-                Long.parseLong(cleanedData[3])
-        );
+
+        String stringContentOfFile = bld.toString();
+
+        String[] parsedArg = new String[4];
+        String[] keyVals = stringContentOfFile.split("\n");
+        for (int i = 0; i < keyVals.length; i++) {
+            String[] parts = keyVals[i].split(":",2);
+            parsedArg[i] = parts[1].trim();
+        }
+
+        Profile profile = new Profile(
+                parsedArg[0],
+                Integer.valueOf(parsedArg[1]),
+                parsedArg[2],
+                Long.parseLong(parsedArg[3]));
+        System.out.println(profile);
+        return profile;
     }
 }
-
